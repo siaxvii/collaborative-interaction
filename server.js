9327f15c6d0676f12b8213046c54c0be2b2e336c
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-10-09 04:21:45 -0500

    updates server start-up message

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ io.on("connection", (socket) => {
 //uses port that Heroku provides, or defaults to 3000
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:3000`);
 });
